8b802f183ddaa64246f56b8a4dca7d2e0dc2a46b
Prashant Tripathi <prashant-neural-net@gmail.com>
2026-08-06 02:36:40 +0530

    Initiating JWT authentication

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import time
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import user, post
+from .routers import user, post, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -35,18 +35,10 @@ while True:
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 ## root path
 @app.get("/")
 def root():
     return {"message": "jai mata di"}
-
-
-
-
-
-
-
-
-
--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,12 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def hash(password: str):
     return pwd_context.hash(password)
 
+
 def verify(plain_password, hashed_password):
-    pass
+    return pwd_context.verify(plain_password, hashed_password)
+
+
