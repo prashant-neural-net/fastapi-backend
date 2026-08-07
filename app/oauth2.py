9ce9fc7f4ea007a688c368b32e8ef9e43fb16747
Prashant Tripathi <prashant-neural-net@gmail.com>
2026-08-07 22:16:39 +0530

    oauth

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,31 +1,33 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
-
+from fastapi.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-#secret key
-#algorithm
-#expiration time of token
+# secret key
+# algorithm
+# expiration time of token
 SECRET_KEY = "4ee67ca6a7e5c108ea6964c22897a0fc497b8c800c207c82588a4d3f82c29223"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode,SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
 
 def verify_access_token(token: str, credentials_exception):
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: int = payload.get("user_id")
 
         if id is None:
@@ -35,10 +37,24 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
 
-    return token_data    
+    return token_data
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail=f"could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
-    
-    return verify_access_token(token, credentials_exception)
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Annotated[Session, Depends(database.get_db)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    token_data = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == token_data.id).first()
+
+    return user
+
+
+
+
+
+
